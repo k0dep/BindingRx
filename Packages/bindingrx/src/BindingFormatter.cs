4d8c6bc80cf546e2c74833a762e14636862b81af
starikcetin <cetinsamedtarik@gmail.com>
2019-11-01 20:36:31 +0300

    * created a unity project * moved source files to Packages folder * fixed dependencies to point to git repositories * version bump * use guid option in asmdef

---
+++ Packages/bindingrx/src/BindingFormatter.cs
@@ -0,0 +1,17 @@
+﻿using TypeInspector;
+using UnityEngine;
+
+namespace BindingRx
+{
+    public class BindingFormatter : MonoBehaviour
+    {
+        public object Value
+        {
+            get => null;
+            set => Property.Set(string.Format(Format, value));
+        }
+
+        public string Format = "{0}";
+        public MonoPropertyReference Property;
+    }
+}
